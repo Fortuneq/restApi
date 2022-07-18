797b2cd310fe9282223c61affcedbefd6c2da2ca
Vlad <vladvolkov03@mail.ru>
2022-07-18 17:47:54 +0300

    final progged this shit :hammer:

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"rest_api/internal/app/apiserver"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init(){
+	flag.StringVar(&configPath,"config-path","configs/apiserver.toml","path to config file")
+}
 
 func main(){
-	s := apiserver.New()
-	if err := s.s
+	flag.Parse()
+	config := apiserver.NewConfig()
+	_,err := toml.DecodeFile(configPath,config)
+	if err != nil{
+		log.Fatal(err)
+	}
+	s:= apiserver.New(config)
+	if err:= s.Start();err!=nil{
+		log.Fatal(err)
+	}
 }
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,11 +1,52 @@
 package apiserver
 
-type APIserver struct{}
+import (
+	"io"
+	"net/http"
 
-func New() *APIserver{
-	return &APIserver{}
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+type APIserver struct{
+	config *Config
+	logger * logrus.Logger
+	router * mux.Router
+}
+
+func New(config *Config) *APIserver{
+	return &APIserver{
+		config: config,
+		logger: logrus.New(),
+		router : mux.NewRouter(),
+	}
 }
 
 func (s *APIserver) Start()error{
+	if err := s.configureLogger();err!=nil{
+		return err
+	}
+	s.configureRouter()
+	s.logger.Info("starting api server")
+	return http.ListenAndServe(s.config.BindAddr,s.router)
+}
+
+func(s *APIserver)configureLogger()error{
+	level,err:= logrus.ParseLevel(s.config.LogLevel)
+	if err != nil{
+		return err
+	}
+	s.logger.SetLevel(level)
+
 	return nil
+}
+
+func (s * APIserver)configureRouter(){
+	s.router.HandleFunc("/hello",s.Handlehello())
+}
+
+func (s * APIserver)Handlehello()http.HandlerFunc{
+	return func (w http.ResponseWriter, r * http.Request)  {
+		io.WriteString(w,"Hello")
+	}
 }
